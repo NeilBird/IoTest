e402e8bf821082ef17f95b34827bd76e20ddffd0
Matt Cowen <mattcowen@outlook.com>
2018-10-06 21:03:59 +0100

    Added retry for nic RetryableError problem. Improved check for blob.

--- StackTesting/Run.ps1
+++ StackTesting/Run.ps1
@@ -3,7 +3,8 @@ $cred = Get-Credential -UserName "mcowen" -Message "VM Admin cred"
 
 
 .\RampTest.ps1 -cred $cred `
-            -totalVmCount 1 `
-            -storageKey ($accountKey + '') -resourceGroupNamePrefix 'STD-'
+            -totalVmCount 10 -pauseBetweenVmCreateInSeconds 5 `
+            -storageKey ($accountKey + '') -resourceGroupNamePrefix 'STB-'
             #-deployArmTemplate
 
+.\ParallelRamp.ps1 -pauseBetweenRampInSeconds 60 -root . -cred $cred -accountKey ($accountKey + '')
