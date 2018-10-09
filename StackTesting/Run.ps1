23688b752c1b3475a543cedbde217539ee444aee
Matt Cowen <mattcowen@outlook.com>
2018-10-09 23:17:53 +0100

    Fix for warning in diskspd surfaced as error

--- StackTesting/Run.ps1
+++ StackTesting/Run.ps1
@@ -1,7 +1,10 @@
 $cred = Get-Credential -UserName "mcowen" -Message "VM Admin cred"
 
 
-.\RampTest.ps1 -cred $cred -totalVmCount 5 -pauseBetweenVmCreateInSeconds 1 `
-            -resourceGroupNamePrefix 'STA-' -password $cred.Password -initialise -dontDeleteResourceGroupOnComplete
+.\RampTest.ps1 -cred $cred -totalVmCount 1 -pauseBetweenVmCreateInSeconds 1 -location 'mas2' -vmsize 'Standard_F4s_v2' `
+    -storageUrlDomain 'blob.mas2.mtc-tvp.com' -testParams '-c200M -t2 -o20 -d30 -w50 -Rxml' -dataDiskSizeGb 128 `
+     -resourceGroupNamePrefix 'STA-' -password $cred.Password #-initialise
 
 .\ParallelRamp.ps1 -pauseBetweenRampInSeconds 60 -root . -cred $cred -accountKey ($accountKey + '')
+
+
