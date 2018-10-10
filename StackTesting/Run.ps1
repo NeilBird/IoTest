2793b820dc0a0c954200a5d5c12bac03690a1b81
Matt Cowen <mattcowen@outlook.com>
2018-10-10 13:06:15 +0100

    API version issue affecting upload to Stack blob fixed

--- StackTesting/Run.ps1
+++ StackTesting/Run.ps1
@@ -2,8 +2,8 @@ $cred = Get-Credential -UserName "mcowen" -Message "VM Admin cred"
 
 
 .\RampTest.ps1 -cred $cred -totalVmCount 1 -pauseBetweenVmCreateInSeconds 1 -location 'mas2' -vmsize 'Standard_F4s_v2' `
-    -storageUrlDomain 'blob.mas2.mtc-tvp.com' -testParams '-c200M -t2 -o20 -d30 -w50 -Rxml' -dataDiskSizeGb 128 `
-     -resourceGroupNamePrefix 'STA-' -password $cred.Password #-initialise
+    -storageUrlDomain 'blob.mas2.mtc-tvp.com' -testParams '-c250M -t2 -o20 -d30 -w50 -Rxml' -dataDiskSizeGb 128 `
+     -resourceGroupNamePrefix 'STB-' -password $cred.Password 
 
 .\ParallelRamp.ps1 -pauseBetweenRampInSeconds 60 -root . -cred $cred -accountKey ($accountKey + '')
 
